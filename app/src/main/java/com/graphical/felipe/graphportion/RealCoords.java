d4c0d70f2c3d8984120b850efd37a7e6b1463d2e
Felipe <feliperod0519@hotmail.com>
2015-06-05 01:23:43 -0400

    Initial commit

---
+++ app/src/main/java/com/graphical/felipe/graphportion/RealCoords.java
@@ -0,0 +1,33 @@
+package com.graphical.felipe.graphportion;
+
+public class RealCoords {
+
+    private float x = 0;
+    private float y = 0;
+
+    public RealCoords(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX()
+    {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x)
+    {
+        this.x = x;
+    }
+
+    public void setY(float y)
+    {
+        this.y = y;
+    }
+
+}
